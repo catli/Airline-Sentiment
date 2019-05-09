248bffa13f1ea05fc927cb7e1ce5255dededda49
Cathleen Li <cathleenlying@gmail.com>
2019-05-09 10:45:07 -0700

    complete analysis and prediction

---
+++ model/process_data.py
@@ -0,0 +1,158 @@
+'''
+    Process stop words and labels
+'''
+
+
+from nltk.corpus  import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+import pandas as pd
+import pickle as pk
+import numpy as np
+import os
+# testing
+import pdb
+
+
+class ProcessData:
+
+    def __init__(self, data):
+        self.data = data
+
+
+    def createTrainData(self):
+        '''
+            process data with the following steps:
+                (1) remove stop words and common punctuation, set to lower case
+                (2) tokenize words into integers, store index in tokenizer
+                (3) create one hot vector for data label
+        '''
+        # uncomment to include stop words
+        # self.removeStopWords()
+        padded_sequence, tokenizer = self.createTokenizeData()
+        labels = self.createDataLabel()
+        return padded_sequence, tokenizer, labels
+
+    def removeStopWords(self):
+        '''
+            remove the stop words and common punctuation from each tweet
+            and set string to lower-case
+            i.e. "My flight really was the worse." transformed to
+            "flight really worse."
+        '''
+        data = self.data['text']
+
+        stop=stopwords.words('english')
+        punctuation = [',','.',';']
+        print('Removing stop words and punctuations...')
+        for p in punctuation:
+            data = data.map(lambda x: str(x).replace(p, ''))
+        text=[]
+        data = data.map(lambda x: ' '.join(word for word in 
+                                    str(x).lower().split() 
+                                       if word not in set(stop) ))
+        self.data['text'] = data
+
+    def createTokenizeData(self):
+        '''
+            Tokenize sequence into integers
+        '''
+        texts =  [str(string) for string in self.data.text.values]
+        # set high threshold for max number of words to keep
+        tokenizer = Tokenizer(num_words=10000)
+        tokenizer.fit_on_texts(texts)
+        sequences = tokenizer.texts_to_sequences(texts)
+        word_index = tokenizer.word_index
+        padded_sequence = pad_sequences(sequences)
+        return padded_sequence, tokenizer
+
+    def createDataLabel(self):
+        '''
+            create data labels labels
+        '''
+        label_index = {'neutral':1,'negative':2,'positive':0}
+        y_label = self.data['airline_sentiment'].map(label_index)
+        labels = to_categorical(np.asarray(y_label))
+        return labels
+
+
+class createFTEmbedding():
+
+    def __init__(self):
+        '''
+            Initiate the path to fasttext vector files stored locally
+        '''
+        pickle_filename = '~/FastData/wiki.en/wiki.en.pkl'
+        self.pickle_path = os.path.expanduser(pickle_filename)
+
+
+    def processDict(self, tokenizer):
+        '''
+            Load the fast text word dict and then
+            translate to an emedding matrix
+        '''
+        word_vec =  self.loadWordDict()
+        tokenized_word_index = tokenizer.word_index
+        self.creatEmbedMatrix(tokenized_word_index, word_vec)
+
+
+    def loadWordDict(self):
+        '''
+            Load the word dictionary
+        '''
+        pickle_reader = open(self.pickle_path, 'rb')
+        word_vec = pk.load(pickle_reader)
+        return word_vec
+
+
+    def creatEmbedMatrix(self, word_index, ft_word_vec ):
+        '''
+            convert the tokenized word indices
+            into an embedding matrix where the row corresponds to the
+            tokenized index, and the vector directly from fasttext
+        '''
+        # create an embedding matrix
+        num_words = len(word_index) + 1
+        embedding_dim = len(ft_word_vec.get('.'))
+        embedding_matrix = np.zeros((num_words, embedding_dim))
+
+        for word, i in word_index.items():
+            embedding_vector = ft_word_vec.get(word)
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+        self.embedding_matrix = embedding_matrix
+
+
+
+def testStopWords():
+    '''
+        Unit test for stop words
+    '''
+    df = pd.DataFrame({'text': ['Perhaps, we are all pig .',
+            'I have nowhere! ;']})
+    print(df)
+    process = ProcessData(df)
+    process.removeStopWords()
+    print(process.data.text)
+    assert process.data.text[0] == 'perhaps pig'
+    assert process.data.text[1] == 'nowhere!'
+    print('PASS PUNCTUATION AND STOP WORD REMOVAL!')
+
+
+# [TODO] write unit test to ensure toeknization working
+
+# Class create embedding matrix from fasttext
+
+if __name__ == "__main__":
+    testStopWords()
+    sentdata = pd.read_csv('data/train.csv')
+    process = ProcessData(sentdata)
+    padded_sequence, tokenizer = process.createTokenizeData()
+    ft = createFTEmbedding()
+    ft.processDict(tokenizer)
+    embedding_matrix = ft.embedding_matrix
+
+
+
---
+++ model/train.py
@@ -0,0 +1,124 @@
+
+from process_data import ProcessData, createFTEmbedding
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation, Conv1D, MaxPooling1D, Flatten
+from keras.layers.embeddings import Embedding
+from keras.initializers import Constant
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import os
+
+import pandas as pd
+
+import pdb
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+def trainPredModel(data):
+    process = ProcessData(sentdata)
+    padded_sequence, tokenizer, labels = process.createTrainData()
+
+    ft = createFTEmbedding()
+    ft.processDict(tokenizer)
+
+    x_train, x_val, y_train, y_val  = train_test_split(padded_sequence, labels)
+
+    model = compileModel(padded_sequence, labels, tokenizer,
+        ft.embedding_matrix)
+
+    model.fit(x_train, y_train,
+              batch_size=20,
+              epochs=10,
+              validation_data=(x_val, y_val))
+
+    y_pred = model.predict(x_val)
+    translateEvalData(y_pred, y_val, x_val, tokenizer)
+    return model, x_train, x_val, y_train, y_val, y_pred
+
+
+
+def compileModel(padded_sequence, labels, tokenizer, embedding_matrix):
+    '''
+        compile the model that will be used for training
+    '''
+    max_sequence_length = len(padded_sequence[0])
+    num_words = len(tokenizer.word_index) + 1
+    embedding_dim = 300
+    embedding_layer = Embedding(input_dim = num_words,
+                                output_dim = embedding_dim,
+                                embeddings_initializer=Constant(embedding_matrix),
+                                input_length=max_sequence_length,
+                                trainable=False)
+    sequence_input = Input(shape=(max_sequence_length,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+    # x = LSTM(128, dropout=0.2,
+    #     recurrent_dropout=0.2, return_sequences = False)(embedded_sequences)
+    # x = Dropout(0.5)(x)
+    x = Conv1D(50, 5, activation = 'relu')(embedded_sequences)
+    x = MaxPooling1D(5)(x)
+    x = Dropout(0.5)(x)
+    x = Flatten()(x)
+    # Interestingly, adding additional LSTM layer does not seem to help with 
+    x = Dense(5, activation='relu')(x)
+    preds = Dense( labels.shape[1], activation='softmax')(x)
+
+    model = Model(sequence_input, preds)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['acc'])
+    return model
+
+
+def translateEvalData(y_pred, y_val, x_val, tokenizer):
+    '''
+        convert the prediction and actual values to readable form and write
+        to file
+    '''
+    back_index = {}
+    for key, val in tokenizer.word_index.items():
+        back_index[val] = key
+    pred_label = pd.DataFrame(y_pred).apply(lambda x: x.idxmax(), axis=1)
+    y_label = pd.DataFrame(y_val).apply(lambda x: x.idxmax(), axis=1)
+    is_correct = y_label==pred_label
+
+    tabulatePrediction(y_label, is_correct)
+
+    output_writer = open('eval_prediction.csv', 'w')
+
+    output_writer.write('actual sentiment' + '\t' +
+                    'predicted sentiment' + '\t' +
+                    'words' + '\t' +
+                    'correct' + '\n')
+
+    for pred, y, x, b in zip(pred_label, y_label, x_val, is_correct):
+        words = translateArrToWords(x, back_index)
+        output_writer.write(str(y) + '\t' +
+               str(pred) + '\t' +
+               str(words) + '\t' +
+               str(b) + '\n' )
+
+def tabulatePrediction(y_label, is_correct):
+    '''
+        percent correct prediction by actual label
+    '''
+    prediction = pd.DataFrame({'actual': y_label, 'is_correct': is_correct})
+    print('PREDICTION TABULATION')
+    print(prediction.groupby(['actual']).mean())
+
+
+def translateArrToWords(arr, back_index):
+    '''
+        function to translate tokenized indices back to human-readable words
+    '''
+    words = ''
+    for ind in arr:
+        words = words + str((back_index[ind] + ' ') if ind!=0 else '')
+    return words
+
+
+if __name__ == "__main__":
+    sentdata = pd.read_csv('data/train.csv')
+    model, x_train, x_val, y_train, y_val, y_pred = trainPredModel(sentdata)
+
+
+
